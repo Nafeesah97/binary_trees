21de119a0624db3b3941241bc7468dec3ecabb1e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-02 21:27:04 +0100

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,6 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t i, j;
-	const binary_tree_t *second, *first;
-
 	if (tree == NULL)
 		return (0);
 	return (1 + binary_tree_size(tree->left) +
