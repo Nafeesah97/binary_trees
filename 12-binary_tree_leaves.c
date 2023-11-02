fa1014a190813a31fdb18bd72f1623074f5792cf
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-02 22:33:07 +0100

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,9 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->parent != NULL)
-		return (1);
-	if (tree->right == NULL && tree->parent != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	i = binary_tree_leaves(tree->left);
 	j = binary_tree_leaves(tree->right);
