d0a1b67e9a5b17ee93ccb7ee17ac3ed112f685a4
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-02 22:53:21 +0100

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,9 +11,15 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL)
+	{
+		printf("left");
 		return (1);
+	}
 	if (tree->right != NULL)
+	{
+		printf("right");
 		return (1);
+	}
 	return (binary_tree_nodes(tree->left) +
 	binary_tree_nodes(tree->right));
 }
